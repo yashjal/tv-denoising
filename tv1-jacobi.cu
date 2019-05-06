be39785d820670907acd8cdf7615610cc31208b0
Yash Jalan <yj627@cuda3.cims.nyu.edu>
2019-05-05 21:35:34 -0400

    test1

--- tv1-jacobi.cu
+++ tv1-jacobi.cu
@@ -97,8 +97,8 @@ __global__ void GPU_jacobi(float* u0, float* u1, float *f, float* err, long Xsiz
 
 int main() {
   //long repeat = 500;
-  long T = 1; // total variation 
-  long N = 1000; // jacobi
+  long T = 100; // total variation 
+  long N = 10000; // jacobi
   float eps = 1e-4;
   float del = 1e-4;
   float lambda = 0.5; 
