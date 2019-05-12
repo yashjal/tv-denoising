c56d30a5b42ed2fe9593f0034f6415c012385990
Richard Xu <rx272@cuda3.cims.nyu.edu>
2019-05-12 15:07:55 -0400

    new grad

--- rof-dual.cu
+++ rof-dual.cu
@@ -280,8 +280,8 @@ __global__ void rof(float* u, float* p0x, float* p1x, float* p0y, float* p1y, fl
   u[idx*Ysize + idy] = f[idx*Ysize + idy] + lambda*div[idx*Ysize+idy];
   __syncthreads();
   if (idx < Xsize-1 && idy < Ysize-1) {
-     gradx[idx*Ysize+idy] = (u[(idx+1)*Ysize + idy] - u[(idx)*Ysize + idy]);
-     grady[idx*Ysize+idy] = (u[idx*Ysize + idy+1] - u[idx*Ysize + idy]);  
+     gradx[idx*Ysize+idy] = (u[(idx)*Ysize + idy] - u[(idx-1)*Ysize + idy]);
+     grady[idx*Ysize+idy] = (u[idx*Ysize + idy] - u[idx*Ysize + idy-1]);  
   }
   float numx = p0x[idx*Ysize+idy] + (tau/lambda)*gradx[idx*Ysize+idy];
   float numy = p0y[idx*Ysize+idy] + (tau/lambda)*grady[idx*Ysize+idy];
